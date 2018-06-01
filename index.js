f0bbaf1f359ca01e9fca001b95cd1d1a4ee67148
Brandon Aaskov <brandon@rocketinsights.com>
2018-06-01 00:59:18 -0400

    added a phrasify function

--- index.js
+++ index.js
@@ -20,19 +20,30 @@ const scrapePage = (markup) => {
     const imageUrl = $(this).find('td').eq(1).find('img').attr('src')
     const windDirection = Number(`${imageUrl.replace(/.+(\d.)+\..+/gim, "$1")}0`)
     const speed = $(this).find('td').eq(2).text()
+    const mph = speed.split('Knots')[1].split(' ')[0]
     const temp = trim($(this).find('td').eq(3).text()).split(' ')[2]
 
     data.push({
       altitude,
       windDirection,
-      speed: Number(speed.split('Knots')[1].split(' ')[0]),
-      temp: _.isNaN(Number(temp)) ? 'N/A' : Number(temp)
+      speed: _.round(mph),
+      temp: _.isNaN(Number(temp)) ? null : _.round(temp)
     })
   })
 
   return _.reverse(_.sortBy(data, 'altitude'))
 }
 
+const phrasify = (data) => {
+  const phrase = _.map(data, row => {
+    const { altitude, windDirection, speed, temp } = row
+    const tempPhrase = temp ? `, and is ${temp} degrees` : ''
+    return `At ${altitude} feet, the wind is coming out of ${windDirection} at ${speed} miles per hour${tempPhrase}.`
+  })
+  return phrase.join(' ')
+}
+
 fetchPage()
   .then(scrapePage)
+  .then(phrasify)
   .then(console.log)
